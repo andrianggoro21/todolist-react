e3ba3a9275c4f5595d88c6a4e578268111516ef7
andrianggoro21 <andri.anggoro21@gmail.com>
2023-09-28 22:29:29 +0700

    create todo list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
+import { useState } from "react";
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const [input, setInput] = useState("");
+  const [list, setList] = useState([]);
+  
+  const handleChange = (e) => {
+    setInput(e.target.value)
+  }
 
+  const addTodo = (e) => {
+    e.preventDefault()
+      setList([...list, input]);
+      setInput("");
+      
+  };
+
+  const deleteTodo = (text) => {
+    const newList = list.filter((input) => {
+      return input !== text;
+    });
+    setList(newList);
+  };
+
+  return (
+    <div  className="App">
+      <h1>Todo List</h1>
+      <form className="input-wrapper">
+        <input type="text" name="input" value={input} placeholder="Create a new todo" onChange={handleChange}/>
+        <button onClick={addTodo}>Add</button>
+      </form>
+
+
+      <div className="list">
+        {list?.length > 0 ? (
+          <ul className="todo-list">
+            {list.map((entry, index) => (
+              <div className="todo">
+                <li key={index}> {entry} </li>
+                <button className="delete-button" onClick={() => {deleteTodo(entry);}}>Delete</button>
+              </div>
+            ))}
+          </ul>
+        ) : (
+        <div className="empty">
+          <p>No task found</p>
+        </div>
+      )}
+      </div>
+    </div>
+  )
+    
+}
 export default App;
